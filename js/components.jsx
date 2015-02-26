78b7ec368f3cfc88d07aa63bed7b6ada17315b10
Giancarlo Anemone <ganemone@gmail.com>
2015-02-26 15:53:53 -0500

    Refactor TodoMain into separate file (TodoList)

--- js/components.jsx
+++ js/components.jsx
@@ -10,55 +10,12 @@ var TodoActions = require('./actions');
 var todoListStore = require('./store');
 var _ = require('lodash');
 var TodoItem = require('./components/TodoItem.jsx');
-
-// Renders the todo list as well as the toggle all button
-// Used in TodoApp
-var TodoMain = React.createClass({
-    propTypes: {
-        list: React.PropTypes.arrayOf(React.PropTypes.object).isRequired,
-    },
-    toggleAll: function(evt) {
-        TodoActions.toggleAllItems(evt.target.checked);
-    },
-    render: function() {
-        var filteredList;
-        switch(this.props.showing){
-            case 'all':
-                filteredList = this.props.list;
-                break;
-            case 'completed':
-                filteredList = _.filter(this.props.list,function(item){ return item.isComplete; });
-                break;
-            case 'active':
-                filteredList = _.filter(this.props.list,function(item){ return !item.isComplete; });
-        }
-        var classes = React.addons.classSet({
-            "hidden": this.props.list.length < 1
-        });
-        return (
-            <section id="main" className={classes}>
-                <input id="toggle-all" type="checkbox" onChange={this.toggleAll} />
-                <label htmlFor="toggle-all">Mark all as complete</label>
-                <ul id="todo-list">
-                    {
-                        filteredList.map(function(item) {
-                            return (
-                                <TodoItem
-                                    label={item.label}
-                                    isComplete={item.isComplete}
-                                    key={item.key}/>
-                            );
-                        })
-                    }
-                </ul>
-            </section>
-        );
-    }
-});
+var TodoList = require('./components/TodoList.jsx');
 
 // Renders the headline and the form for creating new todos.
 // Used in TodoApp
-// Observe that the toogleall button is NOT rendered here, but in TodoMain (it is then moved up to the header with CSS)
+// Observe that the toogleall button is NOT rendered here, but in TodoList
+// (it is then moved up to the header with CSS)
 var TodoHeader = React.createClass({
     handleValueChange: function(evt) {
         var text = evt.target.value;
@@ -115,24 +72,24 @@ var TodoFooter = React.createClass({
 
 var CompletedTodos = React.createClass({
     render: function() {
-        return <TodoMain showing="completed" list={this.props.list} />
+        return <TodoList showing="completed" list={this.props.list} />
     }
 });
 
 var ActiveTodos = React.createClass({
     render: function() {
-        return <TodoMain showing="active" list={this.props.list} />
+        return <TodoList showing="active" list={this.props.list} />
     }
 });
 
 var AllTodos = React.createClass({
     render: function() {
-        return <TodoMain showing="all" list={this.props.list} />
+        return <TodoList showing="all" list={this.props.list} />
     }
 });
 
 // Renders the full application
-// activeRouteHandler will always be TodoMain, but with different 'showing' prop (all/completed/active)
+// activeRouteHandler will always be TodoList, but with different 'showing' prop (all/completed/active)
 var TodoApp = React.createClass({
     // this will cause setState({list:updatedlist}) whenever the store does trigger(updatedlist)
     mixins: [Reflux.connect(todoListStore,"list")],
