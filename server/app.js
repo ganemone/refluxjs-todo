db362805bfc85d3e02d72846b671f772fb4a0b8f
Giancarlo Anemone <ganemone@gmail.com>
2015-03-04 14:34:49 -0500

    Implement basic server with in memory todo storage

--- server/app.js
+++ server/app.js
@@ -1,26 +1,70 @@
-var path = require('path');
-var fortune = require('fortune');
-var express = require('express');
+var restify = require('restify');
+var _ = require('lodash');
+// In memory store of todo list
+// Obviously never do this in a real project
+var todos = [];
 
-var app = fortune({
-    db: 'reflux_todo',
-  })
-  .resource('todo', {
-    label: String,
-    created: Date,
-    isComplete: Boolean
-  });
+var server = restify.createServer();
 
-var _app = app.router;
-
-_app.get('/', function(req, res) {
-  res.sendFile(path.join(__dirname, '../public/index.html'));
+server.get('/todos/:id', function(res, req) {
+  var todo = _.findWhere(todos, { id: req.params.id });
+  if (todo) {
+    res.json(todo);
+  }
+  res.status(404);
+  res.end();
 });
 
-// _app.get('bower_components/todomvc-common/base.css', function(req, res) {
-//   res.sendFile(path.join(__dirname, '../bower_components/todomvc-common/base.css'));
-// });
+server.get('/todos/', function(res, req) {
+  res.json(todos);
+});
 
-_app.use(express.static(path.join(__dirname, '../public')));
+server.put('/todos/:id', function(res, req) {
+  var todo = _.findWhere(todos, { id: req.params.id });
+  if (todo) {
+    for(var prop in req.body.data) {
+      if (req.body.data.hasOwnProperty(prop)) {
+        todo[prop] = req.body.data[prop];
+      }
+    }
+    return res.status(200).end();
+  }
+  todo = req.body.data;
+  todo.id = req.params.id;
+  todos.push(todo);
+  res.status(201).end();
+});
 
-app.listen(8080);
+server.patch('/todos/:id', function(res, req) {
+  var todo = _.findWhere(todos, { id: req.params.id });
+  if (todo) {
+    for(var prop in req.body.data) {
+      if (req.body.data.hasOwnProperty(prop)) {
+        todo[prop] = req.body.data[prop];
+      }
+    }
+    return res.status(200).end();
+  }
+  return res.status(204).end();
+});
+
+server.del('/todos/:id', function(res, req) {
+  var found = false;
+  todos = _.filter(todos, function(todo) {
+    if (todo.id === req.params.id) {
+      found = true;
+      return true;
+    }
+    return false;
+  });
+  if (found) {
+    return res.status(200).end();
+  }
+  return res.status(204).end();
+});
+
+server.post('/todos/', function(req, res) {
+  todos.push(req.body);
+  res.status(201);
+  res.end();
+});
--- server/db.js
+++
@@ -1,5 +0,0 @@
-// Type 1: In-memory only datastore (no need to load the database)
-var Datastore = require('nedb');
-var db = new Datastore();
-
-module.exports = db;
